1304ab301ffe969434be49fefe9ab46f0539ce3c
Oscar Rdgz <raadgtooz@gmail.com>
2022-04-03 03:28:59 -0500

    Header v1 done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React, { Component }from 'react'
+import React, { Component }from 'react';
+import paw from './../sources/paw.png';
 
 export default class Header extends Component{
     render(){
         return (
-            <div id='header'>Soy el header</div>
+            <div id='header'>
+                <h1>HENRY DOGS</h1> <img id= 'paw' src= {paw}></img>
+            </div>
         )
     }
 }
