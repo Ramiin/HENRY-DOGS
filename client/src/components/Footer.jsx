cab15e5b214d72985ca5aad1816f1c8abb054452
Oscar Rdgz <raadgtooz@gmail.com>
2022-04-20 17:50:58 -0500

    Final version to deploy withouth warnings

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,13 +10,13 @@ export default function Footer(){
         <div>
         <p>HENRY DOGS </p>
         <p>desarrollado por Oscar Rodriguez.</p>
-        <p style={{fontSize: '10px'}}>ozcardev@gmail.com</p>
+        <p style={{fontSize: '11px'}}>ozcardev@gmail.com</p>
         
         </div>
        
         <div style={{margin:'5px'}}>
-        <a href="https://www.linkedin.com/in/ozcardz/" target="_blank"><img src={linkedin} alt="LinkedIn" />  </a>
-        <a href="https://github.com/Ramiin" target="_blank"><img src={github} alt="GitHub" /> </a>
+        <a href="https://www.linkedin.com/in/ozcardz/" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn" />  </a>
+        <a href="https://github.com/Ramiin" target="_blank" rel="noreferrer"><img src={github} alt="GitHub" /> </a>
         
         </div>
 
